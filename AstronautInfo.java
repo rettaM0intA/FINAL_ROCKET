cea80fcc43d94af1d094f5e9519a8c1274ead419
rettaM0intA <vansnepsonc23@gmail.com>
2022-04-05 12:52:10 -0400

    Created beginning templates

---
+++ AstronautInfo.java
@@ -0,0 +1,12 @@
+public class AstronautInfo{
+
+    //Create variables here
+
+    /**
+     * Put information for the rocket inside.
+     * @param input First input
+     */
+    public AstronautInfo(String input){
+        
+    }
+}
---
+++ RocketInfo.java
@@ -0,0 +1,13 @@
+public class RocketInfo {
+    
+    //Create variables here
+
+    /**
+     * Put information for the rocket inside.
+     * @param input First input
+     */
+    public RocketInfo(String input){
+
+    }
+
+}
