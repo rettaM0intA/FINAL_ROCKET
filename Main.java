7398c938a94430a2e6181937102adefa1f077613
rettaM0intA <vansnepsonc23@gmail.com>
2022-04-07 12:45:19 -0400

    Started work

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
     
     
-    public static void main(String[] args) {
     
+    public static void main(String[] args) {
+
+        PasswordCreator passwordCreator = new PasswordCreator();
+
     }
 
 }
---
+++ PasswordCreator.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class PasswordCreator {
+
+    String admindPassword = "";
+    
+    public PasswordCreator(){
+           
+    }
+
+    public boolean CreateAdminPassword(){
+        return false;
+    }
+
+
+}
