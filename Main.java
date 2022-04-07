2e056178c956d46c7d63720970dea21e8001a7a5
rettaM0intA <vansnepsonc23@gmail.com>
2022-04-07 14:11:33 -0400

    Started Password project
    Created basic version of password generation logic and put in some experimental code that would work with the Encryption files. The experimental code is commented out.

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
 public class Main {
     
-    
-    
     public static void main(String[] args) {
+
+        
+        String astroFileName = "AtronautInformation.dat";
+        String rocketFileName = "RocketInformation.dat";
+        String passwordsFileName = "Passwords.dat";
+
+        try {
+            // Encryptor encryptor = new Encryptor(astroFileName, rocketFileName, passwordsFileName);
+        } catch (Exception FileAlreadyExistsException) {
+            System.out.print(FileAlreadyExistsException.getMessage());
+        }
 
         PasswordCreator passwordCreator = new PasswordCreator();
 
--- PasswordCreator.java
+++ PasswordCreator.java
@@ -1,16 +1,38 @@
 import java.util.Random;
 
+// import Enums.FileSelect;
+
 public class PasswordCreator {
 
-    String admindPassword = "";
+    String adminPassword = "";
+    Random random = new Random();
     
     public PasswordCreator(){
            
     }
 
-    public boolean CreateAdminPassword(){
+    /*
+    public boolean CreateAdminPassword(Encryptor encryptor){
+
+        String password = encryptor.getUnencrypted(FileSelect.password, 0);
+
+        if(password.charAt(0) == 'E' && password.charAt(1) == 'R' && password.charAt(2) == 'R' && password.charAt(3) == 'O' && password.charAt(4) == 'R'){
+            GenerateAdminPassword();
+            encryptor.toEncrypt(adminPassword, FileSelect.password);
+        }
+
         return false;
     }
+    */
 
+    private void GenerateAdminPassword(){
+        int length = random.nextInt(70) + 19;
+        adminPassword = "";
+
+        for(int i = 0; i < length; i++){
+            adminPassword += (char) (random.nextInt(26) + 'a');
+        }
+
+    }
 
 }
