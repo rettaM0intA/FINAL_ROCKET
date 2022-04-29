68de22f737a02eae68705566e7e46a8bcc6cd2ee
Angeltheo <103059716+Angeltheo@users.noreply.github.com>
2022-04-29 14:42:24 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,5 +7,87 @@ public class Main {
     public static void main(String[] args) {
         userInput.newAstronaut();
     }
+    /*
+    import java.util.Scanner;
+import java.io.*;  //needed for file class
+
+public class Main {
+    public static void main(String[] args{
+
+        Scanner keyboard = new Scanner(System.in);
+        String beginInput;
+        double secondInput;
+        //Two different input??? or should I use the same one?
+
+
+        System.out.println("-Welcome to the SpaceZ space program-\nWould you like to: \n1-Login \n2-Create account");
+        beginInput = keyboard.nextLine();
+
+        switch (beginInput){
+            case 1:
+            System.out.println("Enter Password:");
+            //Enter passwords and get accepted
+            //Make password v
+            break;
+
+            case 2:
+            System.out.println("-Example text_");
+            // How do you create a account that the program can save and recongize?
+            break;
+
+            default: 
+            System.out.println("Please enter either 1 or 2");
+            // should it say somthing more professional? 
+            break;
+
+        }
+        
+        
+        
+        System.out.println("Please enter a number to where you want to go:")
+        // different wording??
+        System.out.println("1-Astronaut Data \n2-Rocket Data \n3-Open instruction manual \n4-Close program");
+        secondInput = keyboard.nextDouble();
+
+        //make is so when a certain number is selected it sends the user to the selected program
+        // just example input for now
+        // needs a way to send the user back if wanted (go back or undo)
+
+        switch (secondInput){
+            case 1:
+            System.out.println("What would you like to do? \nAccess data \nEdit data");
+            // sends the user to the information or a way to edit the info
+            // Change text???
+            break;
+
+            case 2:
+            System.out.println("What would you like to do? \nAccess data \nEdit data");
+            // sends use to rocket information file
+            break;
+
+            case 3:
+            public class ReadTextAsString{
+            
+            }
+            
+            break;
+
+            case 4:
+            System.out.println("You entered 4.");
+            keyboard.close();
+            break;
+            // closes program
+
+            default:
+            System.out.println("Please enter a number between 1-4");
+
+        }
+
+        
+        
+        
+    }
+
+    */
 
 }
