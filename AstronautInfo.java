b27fccbee5d333706a57184ca66c932efae42d2f
rettaM0intA <vansnepsonc23@gmail.com>
2022-05-02 12:21:19 -0400

    b

---
+++ AstronautInfo.java
@@ -0,0 +1,17 @@
+public class AstronautInfo{
+
+    //Create variables here
+    public String fileName = "AtronautInformation.dat";
+
+    /**
+     * Put information for the rocket inside.
+     * @param input First input
+     */
+    public AstronautInfo(){
+        
+    }
+
+    public void CreateAstronaut(String[] inputs){
+        
+    }
+}
